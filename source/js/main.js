b5e0ab91d88415d1efc621c73388204fa40b2ffc
Anton Trochuk <trochuk19@gmail.com>
2025-04-10 23:15:54 +0400

    Разметка блока header (стили + js)

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,7 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+import { wrapperMenu } from './wrapper-menu';
+
+wrapperMenu ();
