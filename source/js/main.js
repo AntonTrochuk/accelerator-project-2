b735cd7ab741564fcbada371b9ae5c5e39a33d3a
Anton Trochuk <trochuk19@gmail.com>
2025-04-25 12:52:42 +0400

    Валидация формы

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,9 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
 
 import { wrapperMenu } from './wrapper-menu';
+import { formValid } from './form-valid';
 
 wrapperMenu ();
+formValid();
 
 function updateSlideFocus(slider) {
   slider.slides.forEach((slide, index) => {
